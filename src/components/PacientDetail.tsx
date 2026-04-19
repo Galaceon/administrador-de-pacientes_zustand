422dfadcbbdc671968d0af39bbafa12cb5d4a1b2
Galaceon <antoniogarcialtr@gmail.com>
2026-04-19 20:22:55 +0200

    diplaying patient info

--- src/components/PacientDetail.tsx
+++ src/components/PacientDetail.tsx
@@ -1,4 +1,5 @@
 import type { Pacient } from "../types"
+import PacientDetailItem from "./PacientDetailItem"
 
 type PacientDetailProps = {
     pacient: Pacient
@@ -6,6 +7,13 @@ type PacientDetailProps = {
 
 export default function PacientDetail({pacient} : PacientDetailProps) {
   return (
-    <div>{pacient.name}</div>
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+        <PacientDetailItem label="ID" data={pacient.id} />
+        <PacientDetailItem label="Nombre" data={pacient.name} />
+        <PacientDetailItem label="Cuidador" data={pacient.caretaker} />
+        <PacientDetailItem label="Email" data={pacient.email} />
+        <PacientDetailItem label="Fecha Alta" data={pacient.date.toString()} />
+        <PacientDetailItem label="Síntomas" data={pacient.symptoms} />
+    </div>
   )
 }
